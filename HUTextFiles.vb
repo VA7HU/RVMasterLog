af4e4bb741b6e66387b29b5bb75f43fa0351c5fe
Hew Lines <va7hu@shaw.ca>
2017-04-09 17:01:52 -0700

    Work on HUTextFIles

--- HUTextFiles.vb
+++ HUTextFiles.vb
@@ -23,25 +23,33 @@ Public Class HUSettingsFile
   '========================================================================================
   Dim cstrFileTypeConfig As String = "CONFIG"
   Dim cstrFileTypeData As String = "DATA"
-  Dim cstrFileTypeSettings As String = "SETTINGS"
+  Dim cstrFileTypeNone As String = ""
   Dim cstrFileTypeOther As String = "OTHER"
+  Dim cstrFileTypeSettings As String = "SETTINGS"
 
-  Dim cstrConfigFileExyension As String = ".cfg"
-  Dim cstrSettingsFileExtenson As String = ".stg"
-  Dim cstrDataFileExtenson As String = ".dta"
+  Dim cstrDefaultConfigFileName = "Config"
+  Dim cstrDefaultDataFileName = "Data"
+  Dim cstrDefaultSettingsFileName = "Settings"
+
+
+  Dim cstrConfigFileExtension As String = ".cfg"
+  Dim cstrDataFileExtension As String = ".dta"
+  Dim cstrSettingsFileExtension As String = ".stg"
 
   Dim cstrDefaultFileExtension As String = cstrSettingsFileExtenson
+  Dim cstrDefaultFileName As String = cstrDefaultSettingsFileName
 
   '========================================================================================
   '          PUBLIC CONSTANTS
-  '============================= ===================================================
+  '========================================================================================
 
   '========================================================================================
   '          PRIVATE VARIABLES
   '========================================================================================
-  Dim vstrFileExtension As String
+  Dim vstrNewFileExtension As String
   Dim vstrNewFileName As String
-
+  Dim vstrNewFilePath = As string
+  
   '========================================================================================
   '          PUBLIC VARIABLES
   '========================================================================================
@@ -85,32 +93,80 @@ Public Class HUSettingsFile
   '========================================================================================
   '          PUBLIC ROUTINES
   '========================================================================================
-  Public Function OpenHUSettingsFile(vstrFileType As String,
-                             vstrFileName As String,
-                            vblnCreate As Boolean) As Boolean
-
-    Dim FileNameParameterExt As String
-    Dim FileNameParameter As String
-
-    'Ensure that the FileName parameter has been passed
-
-    If Len(vstrFileName) < 1 Then
-      MessageBox.Show("No FIlename Parameter")
-      Return False
-    End If ' If Len(vstrFileName) < 1
-
-    ' Now we must determine the new File Extension using both the FileType and FileName 
-    ' parameters.
-    '   Case 1 FileType - Use FileType
-    '   Case 2 No FileType but FileName - use FileName
-    '   Case 3 No FileType and No FileName - use Default
+  Public Function OpenHUTextFile(vstrFileType As String) As String
+    'Creates and Opens a default text file based on the FileType parameter passed.
+    ' The default FileName will be "Settings.stg" using the application path with a
+    ' Create flag of True. The New FileName is returned to the calling routine.
 
     Select Case vstrFileType
       Case cstrFileTypeConfig
+        vstrNewFileExtension = cstrConfigFileExtension
+        vstrNewFileName = frmAppSettings.ApplicationPath +
+                          "/" +
+                          cstrDefaultConfigFileName +
+                          "." + vstrNewFileExtension
       Case cstrFileTypeData
-      Case cstrFileTypeSettings
+        vstrNewFileExtension = cstrDataFileExtension
+        vstrNewFileName = frmAppSettings.ApplicationPath +
+                          "/" +
+                          cstrDefaultDataFileName +
+                          "." + vstrNewFileExtension
+      Case cstrFileTypeNone
+        vstrNewFileExtension = cstrSettingsFileExtension
+        vstrNewFileName = frmAppSettings.ApplicationPath +
+                          "/" +
+                          cstrDefaultSettingsFileName +
+                          "." + vstrNewFileExtension
       Case cstrFileTypeOther
-    End Select ' Case vstrFileType
+        vstrNewFileExtension = cstrSettingsFileExtension
+        vstrNewFileName = frmAppSettings.ApplicationPath +
+                          "/" +
+                          cstrDefaultSettingsFileName +
+                          "." + vstrNewFileExtension
+      Case cstrFileTypeSettings
+        vstrNewFileExtension = cstrSettingsFileExtension
+        vstrNewFileName = frmAppSettings.ApplicationPath +
+                          "/" +
+                          cstrDefaultSettingsFileName +
+                          "." + vstrNewFileExtension
+    End Select
+
+    Return vstrNewFileName
+
+  End Function ' Public Function OpenHUTextFile
+
+  '----------------------------------------------------------------------------------------
+
+  '  Public Function OpenHUTextFile(vstrFileType As String,
+  '                            vstrFileName As String,
+  '                          vblnCreate As Boolean) As Boolean
+  '
+  'Dim FileNameParameterExt As String
+  'Dim FileNameParameter As String
+
+  'Ensure that the FileName parameter has been passed
+
+  'If Len(vstrFileName) < 1 Then
+  ' MessageBox.Show("No FIlename Parameter")
+  ' Return False
+  'End If ' If Len(vstrFileName) < 1
+
+  ' Now we must determine the new File Extension using both the FileType and FileName 
+  ' parameters.
+  '   Case 1 FileType - Use FileType
+  '   Case 2 No FileType but FileName - use FileName
+  '   Case 3 No FileType and No FileName - use Default
+
+  'Select Case vstrFileType
+  'Case cstrFileTypeConfig
+  'Case cstrFileTypeData
+  'Case cstrFileTypeSettings
+  'Case cstrFileTypeOther
+  'End Select ' Case vstrFileType
+
+
+
+  '
 
 
 
@@ -121,47 +177,43 @@ Public Class HUSettingsFile
 
 
 
+  'Parse the FileName parameter
+  'FileNameParameterExt = Path.GetExtension(vstrFileName)
+  'FileNameParameter = Path.GetFileNameWithoutExtension(vstrFileName)
+
+  'Case 1 There is a FileType so use FileType regardless
+  'If (Len(vstrFileType) > 0) Then
+  ' If the FIleName has an extension, we must replace it'
+  'vstrNewFileName = FileNameParameter + "."
+  'End If
+
+  'Case 2 No File Type parameter and FIleName parameter has an extension so we use the complete
+  ' FileName [arameter
+  'If (Len(vstrFileType) < 1 And (Len(FileNameParameterExt) > 0)) Then
+  'vstrNewFileName = vstrFileName
+  'Return True
+  'End If
+
+
+  '   If Len(vstrFileType) < 1 Then
+
+  '    If Len(vstrFileType) > 0 Then
+  '    vstrFileExtension = vstrFileType
+  '    vstrNewFileName =
+  '    ElseIf Len(Ext) > 0 Then
+  '    Else
+  '    vstrFileExtension = cstrDefaultFileExtension
+  '    End If ' If Len(vstrFileType) > 0
+
+  ' Construct the complete path of the new FileName
+  '    vstrNewFileName = 
 
 
 
 
-    'Parse the FileName parameter
-    FileNameParameterExt = Path.GetExtension(vstrFileName)
-    FileNameParameter = Path.GetFileNameWithoutExtension(vstrFileName)
+  'Return True
 
-    'Case 1 There is a FileType so use FileType regardless
-    If (Len(vstrFileType) > 0) Then
-      ' If the FIleName has an extension, we must replace it
-      vstrNewFileName = FileNameParameter + "."
-    End If
-
-    'Case 2 No File Type parameter and FIleName parameter has an extension so we use the complete
-    ' FileName [arameter
-    If (Len(vstrFileType) < 1 And (Len(FileNameParameterExt) > 0)) Then
-      vstrNewFileName = vstrFileName
-      Return True
-    End If
-
-
-    '   If Len(vstrFileType) < 1 Then
-
-    '    If Len(vstrFileType) > 0 Then
-    '    vstrFileExtension = vstrFileType
-    '    vstrNewFileName =
-    '    ElseIf Len(Ext) > 0 Then
-    '    Else
-    '    vstrFileExtension = cstrDefaultFileExtension
-    '    End If ' If Len(vstrFileType) > 0
-
-    ' Construct the complete path of the new FileName
-    '    vstrNewFileName = 
-
-
-
-
-    Return True
-
-  End Function ' Public Function OpenHUSettingsFile
+  'End Function ' Public Function OpenHUTextFile
 
   '========================================================================================
   '          FILE ROUTINES
