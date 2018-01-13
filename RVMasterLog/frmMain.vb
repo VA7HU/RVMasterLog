666348f06bbec0af3964e2be308ee4b156d9f3a5
va7hu <va7hu@shaw.ca>
2018-01-13 15:51:07 -0800

    Work on frmMain

--- RVMasterLog/frmMain.vb
+++ RVMasterLog/frmMain.vb
@@ -14,15 +14,15 @@
   '
   '   Version: 1.0.0
   '
-  '   Date: 19 May 2017
+  '   Date: 13 Jan 2018
   '
   '========================================================================================
 
   '========================================================================================
   '          PRIVATE CONSTANTS
   '========================================================================================
-  'Private cstrAppClosingText = "RVMasterLog Closing"
-  'Private cstrAppClosingTitle = "Application Closing"
+  Private cstrAppClosingText = "RVMasterLog Closing"
+  Private cstrAppClosingTitle = "Application Closing"
 
   '========================================================================================
   '          PUBLIC CONSTANTS
@@ -55,28 +55,24 @@
   '========================================================================================
   '         FILE MENU
   '========================================================================================
-  'Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
-  '  Application.Exit()
-  'End Sub 'Private Sub mnuFileExit_Click
+  Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+    MessageBox.Show("mnuFileExit_Click")
+    Application.Exit()
+  End Sub 'Private Sub mnuFileExit_Click
 
   '========================================================================================
   '         SETTINGS MENU
   '========================================================================================
-  'Private Sub mnuSettings_Click(sender As Object, e As EventArgs) Handles mnuSettings.Click
-  '  frmAppSettings.Show()
-  'End Sub 'Private Sub mnuSettings_Click
+  Private Sub mnuSettings_Click(sender As Object, e As EventArgs) Handles mnuSettings.Click
+    frmAppSettings.Show()
+  End Sub 'Private Sub mnuSettings_Click
 
   '========================================================================================
   '         CONFIGURATION MENU
   '========================================================================================
-  'Private Sub mnuConfigurationDatabaseRVTypes_Click(sender As Object, e As EventArgs)
-  '  frmRVMDefaultTablesDB.ShowDialog()
-  'End Sub ' Private Sub mnuConfigurationDatabaseRVTypes_Click
-
-  '----------------------------------------------------------------------------------------
-  'Private Sub DefaultRVTypeTableToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DefaultRVTypeTableToolStripMenuItem.Click
-  '  frmRVMDefaultTablesDB.Show()
-  'End Sub ' Private Sub DefaultRVTypeTableToolStripMenuItem_Click
+  Private Sub mnuConfigureRVTypesTable_Click(sender As Object, e As EventArgs) Handles mnuConfigureRVTypesTable.Click
+    frmRVMDefaultTablesDB.Show()
+  End Sub ' Private Sub mnuConfigureRVTypesTable_Click
 
   '========================================================================================
   '          COMMAND BUTTON ROUTINES
@@ -93,26 +89,26 @@
   '========================================================================================
   '          FORM ROUTINES
   '========================================================================================
-  'Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-  '  If Not AppInit.Initialise() Then
-  '    Me.Close()
-  '  End If 'If Not AppInit.Initialise
-  'End Sub 'Private Sub frmMain_Load
+  Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    If Not AppInit.Initialise() Then
+      Me.Close()
+    End If 'If Not AppInit.Initialise
+  End Sub 'Private Sub frmMain_Load
 
   '========================================================================================
-  'Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-  '  'MessageBox.Show("RVMasterLog Closing")
-  '  MessageBox.Show(cstrAppClosingText,
-  '                    cstrAppClosingTitle,
-  '                    MessageBoxButtons.OK,
-  '                    MessageBoxIcon.Information)
-  '  AppFinal.Finalise()
-  'End Sub ' Private Sub frmMain_FormClosing
+  Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+    'MessageBox.Show("RVMasterLog Closing")
+    MessageBox.Show(cstrAppClosingText,
+                      cstrAppClosingTitle,
+                      MessageBoxButtons.OK,
+                      MessageBoxIcon.Information)
+    AppFinal.Finalise()
+  End Sub ' Private Sub frmMain_FormClosing
 
   '----------------------------------------------------------------------------------------
-  'Private Sub frmMain_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-  '  MessageBox.Show("Form Cosed")
-  'End Sub ' Private Sub frmMain_FormClosed
+  Private Sub frmMain_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+    MessageBox.Show("Form Cosed")
+  End Sub ' Private Sub frmMain_FormClosed
 
   '========================================================================================
 End Class
