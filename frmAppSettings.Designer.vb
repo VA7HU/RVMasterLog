447edac3987dc4e351860f715e738da7ac593268
Hew Lines <va7hu@shaw.ca>
2018-02-28 09:21:31 -0800

    Replaced frmAppSettings

---
+++ frmAppSettings.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAppSettings
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAppSettings))
+    Me.btnCancle = New System.Windows.Forms.Button()
+    Me.SuspendLayout()
+    '
+    'btnCancle
+    '
+    Me.btnCancle.Image = CType(resources.GetObject("btnCancle.Image"), System.Drawing.Image)
+    Me.btnCancle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+    Me.btnCancle.Location = New System.Drawing.Point(486, 317)
+    Me.btnCancle.Name = "btnCancle"
+    Me.btnCancle.Size = New System.Drawing.Size(75, 23)
+    Me.btnCancle.TabIndex = 0
+    Me.btnCancle.Text = "     &Cancel"
+    Me.btnCancle.UseVisualStyleBackColor = True
+    '
+    'frmAppSettings
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(607, 375)
+    Me.Controls.Add(Me.btnCancle)
+    Me.Name = "frmAppSettings"
+    Me.Text = "frmAppSettings"
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents btnCancle As Button
+End Class
--- frmAppSettings.vb
+++ frmAppSettings.vb
@@ -1,22 +1,18 @@
-﻿Public Class frmMain
+﻿Public Class frmAppSettings
 
   '========================================================================================
   '
-  '   Filename : frmMain.vb
+  '   Filename : frmAppSettings.vb
   '
-  '   Description: Application Main module
+  '   Description: 
   '
   '   Called By:  
   '
-  '   Calls:  AppFinal  : Finalise
-  '                       AppInit: Initialise
-  '           frmAppSettings : Show
-  '           frmRVTypesDB : frmRVTypesDB.Show
-  '           Libraries : 
+  '   Calls: 
   '
   '   Version: 1.0.0
   '
-  '   Date: 25 Feb 2018
+  '   Date: 28 Feb 2018
   '
   '========================================================================================
 
@@ -55,24 +51,14 @@
   '========================================================================================
   '         FILE MENU
   '========================================================================================
-  Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
-    Libraries.HUMsgLib.HUInformationMessageOK("File Exit Click", Nothing)
-    Application.Exit()
-  End Sub 'Private Sub mnuFileExit_Click
 
   '========================================.================================================
   '         SETTINGS MENU
   '========================================================================================
-  Private Sub mnuSettings_Click(sender As Object, e As EventArgs) Handles mnuSettings.Click
-    frmAppSettings.Show()
-  End Sub 'Private Sub mnuSettings_Click
 
   '========================================================================================
   '         CONFIGURATION MENU
   '========================================================================================
-  Private Sub mnuConfigureRVTypesTable_Click(sender As Object, e As EventArgs) Handles mnuConfigureRVTypesTable.Click
-    'frmRVMDefaultTablesDB.Show()
-  End Sub ' Private Sub mnuConfigureRVTypesTable_Click
 
   '========================================================================================
   '          COMMAND BUTTON ROUTINES
@@ -89,22 +75,8 @@
   '========================================================================================
   '          FORM ROUTINES
   '========================================================================================
-  Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    If Not AppInit.Initialise() Then
-      Me.Close()
-    End If 'If Not AppInit.Initialise
-  End Sub 'Private Sub frmMain_Load
 
   '========================================================================================
-  Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-    Libraries.HUMsgLib.HUInformationMessageOK("RVMasterLog Closing", Nothing)
-    AppFinal.Finalise()
-  End Sub ' Private Sub frmMain_FormClosing
-
-  '----------------------------------------------------------------------------------------
-  Private Sub frmMain_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-    Libraries.HUMsgLib.HUInformationMessageOK("RVMasterLog Closed", Nothing)
-  End Sub ' Private Sub frmMain_FormClosed
 
   '========================================================================================
 End Class 'Public Class frmMain
