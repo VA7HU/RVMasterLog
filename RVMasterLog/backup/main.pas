756268ea8448e424c5f776c4e843c7812c7fbdf3
Hew Lines <va7hu@shaw.ca>
2018-07-29 15:44:00 -0700

    Conversion to Lazarus

---
+++ RVMasterLog/backup/main.pas
@@ -0,0 +1,26 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMAin = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmMAin: TfrmMAin;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
