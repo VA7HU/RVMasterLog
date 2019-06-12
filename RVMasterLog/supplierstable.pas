e2faf2474a996819cec9ea1dc4afc2c59c592e58
Hew Lines <va7hu@shaw.ca>
2019-06-12 10:23:34 -0700

    Added KeyPress toutines to SuppliersTable

--- RVMasterLog/supplierstable.pas
+++ RVMasterLog/supplierstable.pas
@@ -10,12 +10,17 @@ unit SuppliersTable;
 //
 // Called By : AppSettings : frmSettings.CreateUserDataDirectories
 //
-// Calls : frmAppSettings.pApplicationDB
+// Calls : AppSettings : frmAppSettings.pApplicationDB
+//         HUValidations : VaildAddressCharacter
+//                         ValidNameCharacter
+//                         ValidAddressCharacter
+//                         ValidCityCharacter
+//                         ValidProvStateCharacter
 //         Main : TerminateApp
 //
 // Ver. : 1.0.0
 //
-// Date : 10 Jun 2019
+// Date : 12 Jun 2019
 //
 //========================================================================================
 
@@ -23,10 +28,10 @@ interface
 
 uses
   Classes, FileUtil, SysUtils, sqlite3conn, sqldb, db, Forms, Controls,
-  Graphics, Dialogs, Buttons, DBCtrls, DBGrids, StdCtrls, ComCtrls, ExtCtrls;
+  Graphics, Dialogs, Buttons, DBCtrls, DBGrids, StdCtrls, ComCtrls, ExtCtrls,
   // Application Units
   // HULib Units
-
+  HUValidations;
 
 //========================================================================================
 //          PUBLIC VARIABLES
@@ -87,7 +92,9 @@ type
     SupplierTableQuery: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
     procedure dbeAddress1Change(Sender: TObject);
+    procedure dbeAddress1KeyPress(Sender: TObject; var Key: char);
     procedure dbeAddress2Change(Sender: TObject);
+    procedure dbeAddress2KeyPress(Sender: TObject; var Key: char);
     procedure dbeC1DeptChange(Sender: TObject);
     procedure dbeC1EmailChange(Sender: TObject);
     procedure dbeC1NameChange(Sender: TObject);
@@ -101,16 +108,19 @@ type
     procedure dbeC3NameChange(Sender: TObject);
     procedure dbeC3PhoneChange(Sender: TObject);
     procedure dbeCityChange(Sender: TObject);
+    procedure dbeCityKeyPress(Sender: TObject; var Key: char);
     procedure dbeGenDeptChange(Sender: TObject);
     procedure dbeGenEmailChange(Sender: TObject);
     procedure dbeGenNameChange(Sender: TObject);
     procedure dbeGenPhoneChange(Sender: TObject);
     procedure dbeIDChange(Sender: TObject);
     procedure dbePostalCodeChange(Sender: TObject);
+    procedure dbePostalCodeKeyPress(Sender: TObject; var Key: char);
     procedure dbeProvStateChange(Sender: TObject);
+    procedure dbeProvStateKeyPress(Sender: TObject; var Key: char);
     procedure dbeSupplierNameChange(Sender: TObject);
-    procedure DBNavigator1BeforeAction(Sender: TObject; Button: TDBNavButtonType
-      );
+    procedure dbeSupplierNameKeyPress(Sender: TObject; var Key: char);
+    procedure DBNavigator1BeforeAction(Sender: TObject; Button: TDBNavButtonType);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -171,145 +181,209 @@ const
 // dbEdit Routines
 //==========
 
+          //==========
+          // On Change
+          //==========
+
 procedure TfrmSuppliersTable.dbeAddress1Change(Sender: TObject);
 begin
   dbeAddress1.Hint := '   ' + dbeAddress1.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeAddress1Change
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeAddress2Change(Sender: TObject);
 begin
   dbeAddress2.Hint := '   ' + dbeAddress2.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeAddress2Change
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC1DeptChange(Sender: TObject);
 begin
   dbeC1Dept.Hint := '   ' + dbeC1Dept.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC1DeptChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC1EmailChange(Sender: TObject);
 begin
   dbeC1Email.Hint := '   ' + dbeC1Email.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC1EmailChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC1NameChange(Sender: TObject);
 begin
   dbeC1Name.Hint := '   ' + dbeC1Name.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC1NameChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC1PhoneChange(Sender: TObject);
 begin
   dbeC1Phone.Hint := '   ' + dbeC1Phone.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC1PhoneChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC2DeptChange(Sender: TObject);
 begin
   dbeC2Dept.Hint := '   ' + dbeC2Dept.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC2DeptChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC2EmailChange(Sender: TObject);
 begin
   dbeC2Email.Hint := '   ' + dbeC2Email.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC2EmailChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC2NameChange(Sender: TObject);
 begin
   dbeC2Name.Hint := '   ' + dbeC2Name.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC2NameChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC2PhoneChange(Sender: TObject);
 begin
   dbeC2Phone.Hint := '   ' + dbeC2Phone.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC2PhoneChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC3DeptChange(Sender: TObject);
 begin
   dbeC3Dept.Hint := '   ' + dbeC3Dept.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC3DeptChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC3EmailChange(Sender: TObject);
 begin
   dbeC3Email.Hint := '   ' + dbeC3Email.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC3EmailChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC3NameChange(Sender: TObject);
 begin
   dbeC3Name.Hint := '   ' + dbeC3Name.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC3NameChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeC3PhoneChange(Sender: TObject);
 begin
   dbeC3Phone.Hint := '   ' + dbeC3Phone.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeC3PhoneChange
 
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeCityChange(Sender: TObject);
 begin
   dbeCity.Hint := '   ' + dbeCity.Text + '   ';
 end;
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeGenDeptChange(Sender: TObject);
 begin
   dbeGenDept.Hint := '   ' + dbeGenDept.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeGenDeptChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeGenEmailChange(Sender: TObject);
 begin
  dbeGenEmail.Hint := '   ' + dbeGenEmail.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeGenEmailChange
 
-//========================================================================================
+//=---------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeGenNameChange(Sender: TObject);
 begin
   dbeGenName.Hint := '   ' + dbeGenName.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeGenNameChange
 
-//========================================================================================
+//=---------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeGenPhoneChange(Sender: TObject);
 begin
   dbeGenPhone.Hint := '   ' + dbeGenPhone.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeGenPhoneChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeIDChange(Sender: TObject);
 begin
   dbeID.Hint := '   ' + dbeID.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeIDChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeProvStateChange(Sender: TObject);
 begin
   dbeProvState.Hint := '   ' + dbeProvState.Text + '   ';
 end;
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbePostalCodeChange(Sender: TObject);
 begin
   dbePostalCode.Hint := '   ' + dbePostalCode.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbePostalCodeChange
 
-//========================================================================================
+//----------------------------------------------------------------------------------------
 procedure TfrmSuppliersTable.dbeSupplierNameChange(Sender: TObject);
 begin
   dbeSupplierName.Hint := '   ' + dbeSupplierName.Text + '   ';
 end;// procedure TfrmSuppliersTable.dbeSupplierNameChange
 
+//========================================================================================
+
+          //==========
+          // On Keypress
+          //==========
+
+procedure TfrmSuppliersTable.dbeSupplierNameKeyPress(Sender: TObject; var Key: char);
+begin
+  Key := ValidNameCharacter(Key);
+end;// procedure TfrmSuppliersTable.dbeSupplierNameKeyPress
+
+//----------------------------------------------------------------------------------------
+procedure TfrmSuppliersTable.dbeAddress1KeyPress(Sender: TObject; var Key: char);
+begin
+  Key := ValidAddressCharacter(Key);
+end;// procedure TfrmSuppliersTable.dbeAddress1KeyPress
+
+//----------------------------------------------------------------------------------------
+procedure TfrmSuppliersTable.dbeAddress2KeyPress(Sender: TObject; var Key: char
+  );
+begin
+  Key := ValidAddressCharacter(Key);
+end;// procedure TfrmSuppliersTable.dbeAddress2KeyPress
+
+//----------------------------------------------------------------------------------------
+procedure TfrmSuppliersTable.dbeCityKeyPress(Sender: TObject; var Key: char);
+begin
+  Key := ValidCityCharacter(Key);
+end;
+
+//----------------------------------------------------------------------------------------
+procedure TfrmSuppliersTable.dbeProvStateKeyPress(Sender: TObject; var Key: char);
+begin
+  Key := ValidProvStateCharacter(Key);
+end;// procedure TfrmSuppliersTable.dbeProvStateKeyPress
+
+//----------------------------------------------------------------------------------------
+procedure TfrmSuppliersTable.dbePostalCodeKeyPress(Sender: TObject;
+  var Key: char);
+begin
+  Key := ValidPostalCodeCharacter(Key);
+end;// procedure TfrmSuppliersTable.dbePostalCodeKeyPress
+
+//----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+//========================================================================================
+
 //==========
 // dbNavigator Routines
 //==========
+
 procedure TfrmSuppliersTable.DBNavigator1BeforeAction(Sender: TObject;
   Button: TDBNavButtonType);
 begin
